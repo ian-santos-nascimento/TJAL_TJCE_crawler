f566060f3d68ccebc7dbaf60bba836ee5be908dc
ian <iansantosnascimento@gmail.com>
2023-09-03 23:15:22 -0400

    feat: Implement basic testing

--- tests/test_tjce_crawler.py
+++ tests/test_tjce_crawler.py
@@ -6,13 +6,13 @@ from scrapy.signalmanager import dispatcher
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.test import get_crawler
 
-from TJAL_crawler.TJAL_crawler.spiders import TjalCrawler
+from TJCE_crawler.TJCE_crawler.spiders import TjceCrawler
 
 
 class TjalCrawlerTest(TestCase):
 
     def setUp(self):
-        self.crawler = get_crawler(TjalCrawler)
+        self.crawler = get_crawler(TjceCrawler)
         self.input_string = "0034520-06.2010.8.02.0001"
         self.spider = self.crawler._create_spider(input_string=self.input_string)
 
@@ -24,12 +24,12 @@ class TjalCrawlerTest(TestCase):
             items.append(item)
 
         process = CrawlerProcess(settings=get_project_settings())
-        crawler = process.create_crawler(TjalCrawler)
+        crawler = process.create_crawler(TjceCrawler)
         dispatcher.connect(collect_items, signal=signals.item_scraped)
         process.crawl(crawler, input_string=self.input_string)
         process.start()
 
-        for item in items:
+        for item in items: #Fields that has to return always
             assert item is not None
             assert item['numero_processo'] is not None
             assert item['grau'] is not None
