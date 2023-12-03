e0f263e4b726672e62d9934e3cac19c48b97ccdc
Ian Santos <iansantosnascimento@gmail.com>
2023-12-02 23:28:13 -0400

    Feat: Implement search in DB before executing crawler and code cleaning

--- courts/api/views.py
+++ courts/api/views.py
@@ -19,18 +19,19 @@ class ApiProcessView(APIView):
     def post(self, request):
         processo = request.data.get('processo')
         tribunal = request.data.get('tribunal')
-        if not processoExiste(processo):
+        if not processExists(processo):
             return Response(data={'error':"Numero do processo inválido!"},status=HTTP_400_BAD_REQUEST)
 
-        task, unique_id = scheduleCrawler(processo,tribunal)
-        print("task" + task)
-        print("UNIQUE_ID" + unique_id)
+        json_data = returnItemFromDb(processo)
+        if json_data:
+            return JsonResponse(json_data, safe=False, status=HTTP_200_OK)
+        task, unique_id = scheduleCrawler(processo, tribunal)
         status = scrapyd.job_status(os.getenv('SCRAPY_PROJECT_NAME'), task)
+
         while status != 'finished':
                 status = scrapyd.job_status(os.getenv('SCRAPY_PROJECT_NAME'), task)
-        items = Processo.objects.filter(numero_processo=processo)
-        serialized_data = list(items.values())
-        json_data = json.dumps(serialized_data, cls=DjangoJSONEncoder)
+
+        json_data = returnItemFromDb(processo)
         return JsonResponse(json_data, safe=False,status=HTTP_200_OK)
 
 
@@ -47,11 +48,18 @@ def scheduleCrawler(processo,tribunal):
                             numero_processo=processo, tribunal=tribunal)
     return task, unique_id
 
+def returnItemFromDb(processo):
+    items = Processo.objects.filter(numero_processo=processo)
+    if items.exists():
+        serialized_data = list(items.values())
+        json_data = json.dumps(serialized_data, cls=DjangoJSONEncoder)
+        return json_data
+    else:
+        return None
 
-
-def processoExiste(numero):
-    padrao = r'^\d{7}-\d{2}\.\d{4}\.\d{1}\.\d{2}\.\d{4}$'
-    if re.match(padrao, numero):
+def processExists(numero):
+    pattern = r'^\d{7}-\d{2}\.\d{4}\.\d{1}\.\d{2}\.\d{4}$'
+    if re.match(pattern, numero):
         return True
     else:
         return False
--- courts/crawler/crawler/pipelines.py
+++ courts/crawler/crawler/pipelines.py
@@ -8,14 +8,6 @@ import json
 logger = logging.getLogger(__name__)
 
 
-def clean_data(data):
-    cleaned_data = {
-        key: [value.strip() for value in values if value.strip()]  # Remove empty or whitespace-only values
-        for key, values in data.items()
-    }
-    return cleaned_data
-
-
 class CrawlerPipeline:
 
     def __init__(self, unique_id, processo, *args, **kwargs):
@@ -32,15 +24,15 @@ class CrawlerPipeline:
 
     async def process_item(self, item, spider):
         self.item = item
-
+        json_string = json.dumps(self.item)
+        # Decode the JSON string to remove escape characters
+        decoded_data = json.loads(json_string)
         my_model_instance = Processo(
             numero_processo=self.processo,
-            data =  self.item
+            data =  decoded_data
         )
 
-        logger.debug(f"my_model_instance {my_model_instance.numero_processo}")
         await self.saveDB(my_model_instance)
-        print("my_model_instance",my_model_instance)
         return item
 
     @sync_to_async
